b9e22be7d32e4d502327470615c0ff056df9c05c
Webforce3 <denis.nerborac@wf3.fr>
2016-05-19 09:59:04 +0200

    FIX posts table => post

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ require_once 'config/config.conf.php';
 
 try {
 
-	$articles = Db::selectAll('SELECT * FROM posts LIMIT 5');
+	$articles = Db::selectAll('SELECT * FROM post LIMIT 5');
 
 	echo Utils::debug($articles);
 
 	echo '<hr>';
 
-	$article = Db::selectOne('SELECT * FROM posts WHERE id = :id', array(
+	$article = Db::selectOne('SELECT * FROM post WHERE id = :id', array(
 		':id' => 23
 	));
 
